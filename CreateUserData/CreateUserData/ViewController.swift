604578b346c13a0f6766fa8fe607420f1e8f3c06
PavunRajTech <104754521+PavunRajTech@users.noreply.github.com>
2022-05-04 16:04:32 +0530

    First Commit

---
+++ CreateUserData/CreateUserData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateUserData
+//
+//  Created by Laptop-OBS-36 on 04/05/22.
+//
+
+import UIKit
+
+import RegisterUserInfo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let user = UserDataInfo()
+        let name = user.getUserDetailInfo()
+        print(name)
+    }
+
+
+}
+
---
+++ CreateUserData/RegisterUserInfo.xcframework/ios-arm64/RegisterUserInfo.framework/Headers/RegisterUserInfo.h
@@ -0,0 +1,18 @@
+//
+//  RegisterUserInfo.h
+//  RegisterUserInfo
+//
+//  Created by Laptop-OBS-36 on 04/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegisterUserInfo.
+FOUNDATION_EXPORT double RegisterUserInfoVersionNumber;
+
+//! Project version string for RegisterUserInfo.
+FOUNDATION_EXPORT const unsigned char RegisterUserInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegisterUserInfo/PublicHeader.h>
+
+
